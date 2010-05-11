a3bc3204e7d2981cc730845b8eb7f1cc885296b2
moonpxi <paulo.schneider@gmail.com>
2010-05-11 13:36:51 +0100

    Adding initial move left and right behavior

--- lib/model.rb
+++ lib/model.rb
@@ -1,9 +1,19 @@
+PAD_VELOCITY = 10
+
 class Pad
   attr_reader :x, :y, :width, :height
 
   def initialize(x, y)
     @x, @y = x, y
     @width, @height = 200, 40
+  end
+
+  def move_left
+    @x -= PAD_VELOCITY
+  end
+
+  def move_right
+    @x += PAD_VELOCITY
   end
 
 end
--- spec/model/pad_spec.rb
+++ spec/model/pad_spec.rb
@@ -2,14 +2,24 @@ require File.dirname(__FILE__) + "/../spec_helper"
 
 describe "Pad" do
 
+  before :each do
+    @pad = Pad.new(200, 532)
+  end
+
   it "should have position and dimensions" do
     # Bit of a lame test.
-    pad = Pad.new(201, 532)
+    @pad.x.should == 200
+    @pad.y.should == 532
+    @pad.width.should == 200
+    @pad.height.should == 40
+  end
 
-    pad.x.should == 201
-    pad.y.should == 532
-    pad.width.should == 200
-    pad.height.should == 40
+  it "should move left and right, according to velocity" do
+    @pad.move_left
+    @pad.x.should == 190
+
+    3.times { @pad.move_right }
+    @pad.x.should == 220
   end
         
 end
